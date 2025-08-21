ad2f42edfc24fb7a0f7001a7999f6b43675614e0
sashaPshonko137 <privetnitron@gmail.com>
2025-08-21 21:45:19 +0500

    kjldaskljsadkljdsa

--- vhod.js
+++ vhod.js
@@ -86,8 +86,8 @@ const msg = message.toLowerCase();
                 console.error(error)
             }
         }
-        bot.message.send(`\nWhoever drops 10g after the word ✅START✅ will receive 20g`).catch(console.error);
-        bot.message.send(`\nПервый, кто скинет 10г после слова ✅START✅ - получит 20г`).catch(console.error);
+        // bot.message.send(`\nWhoever drops 10g after the word ✅START✅ will receive 20g`).catch(console.error);
+        await bot.message.send(`\nПервый, кто скинет 10г после слова ✅START✅ - получит 20г`).catch(console.error);
         await delay(getRandomDelayInRange(5000, 8000))
         if (Math.random() < 0.5) {
             const words = ['✅STRAT✅', "✅STSRT✅", "✅STYRT✅", "✅SPART✅", "✅STUPID✅"]
